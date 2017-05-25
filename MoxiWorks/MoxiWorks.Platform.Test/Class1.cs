a5e03335e641eb0d04823e99f1526f073c4f278d
Anthony Hatch <ahatch1490@gmail.com>
2017-05-25 08:55:50 -0700

    create project framework

---
+++ MoxiWorks/MoxiWorks.Platform.Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace MoxiWorks.Platform.Test
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void shouldbeTrue()
+        {
+            Assert.IsTrue(true); 
+        }
+    }
+}
