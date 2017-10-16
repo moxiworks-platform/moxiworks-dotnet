97df4eacd62dec09d1ab48b3549a58d3a8249c86
Anthony Hatch <ahatch1490@gmail.com>
2017-10-15 23:06:26 -0700

    starting comments for agent

--- MoxiWorks/MoxiWorks.Platform/Agent.cs
+++ MoxiWorks/MoxiWorks.Platform/Agent.cs
@@ -5,13 +5,57 @@ namespace MoxiWorks.Platform
 {
     public class Agent
     {
-       
+        /// <summary>
+        /// This is the Moxi Works Platform ID of the Agent. This will be a string that may take the form 
+        /// of an email address, or a unique alphanumeric identification string.
+        /// </summary>
         [JsonProperty("moxi_works_agent_id")]
         public string MoxiWorksAgentId { get; set; }
+        /// <summary>
+        /// This is the ID of the Agent utilized by their company.
+        /// </summary>
+        [JsonProperty("client_agent_id")]
+        public string ClientAgentId { get; set; }
+        /// <summary>
+        /// This is the ID of this Agent. This will be an integer.
+        /// </summary>
+        [JsonProperty("agent_id")]
+        public string AgentId { get; set; }
+        /// <summary>
+        /// This is the Moxi Works Platform ID of the office for this Agent. This will be an RFC 4122 compliant UUID.
+        /// </summary>
         [JsonProperty("moxi_works_office_id")]
         public string MoxiWorksOfficeId { get; set; }
+        /// <summary>
+        /// This is the ID of the office for this Agent. This will be an integer.
+        /// </summary>
+        [JsonProperty("office_id")]
+        public string OfficeId { get; set; }
+        /// <summary>
+        /// This is the ID of the office for this Agent utilized by their company.
+        /// </summary>
+        [JsonProperty("client_office_id")]
+        public string ClientOfficeId { get; set; }
+        /// <summary>
+        /// This is the ID of the company for this Agent.This will be an integer.
+        /// </summary>
+        [JsonProperty("company_id")]
+        public string CompanyId { get; set; }
+        /// <summary>
+        /// This is the ID of the Company utilized by their company.
+        /// </summary>
+        [JsonProperty("client_company_id")]
+        public string ClientCompanyId { get; set; }
+        /// <summary>
+        /// The street address of the agent’s office. 
+        /// This can be null if there is no data for this attribute.
+        /// </summary>
         [JsonProperty("office_address_street")]
         public string OfficeAddressStreet { get; set; }
+        /// <summary>
+        /// The suite or office number of the agent’s office.This can be null 
+        ///if there is no data for this attribute. 
+        /// </summary>
         [JsonProperty("office_address_street2")]
         public string OfficeAddressStreet2 { get; set; }  
         [JsonProperty("office_address_city")]
