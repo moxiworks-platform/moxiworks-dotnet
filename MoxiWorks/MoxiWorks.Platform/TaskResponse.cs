ca277c3e905e62869810baa502b6837c948855fb
ahatch1490 <ahatch1490@gmail.com>
2017-11-02 15:40:03 -0700

    adding last bit of documentation for tasks

--- MoxiWorks/MoxiWorks.Platform/TaskResponse.cs
+++ MoxiWorks/MoxiWorks.Platform/TaskResponse.cs
@@ -4,12 +4,20 @@ namespace MoxiWorks.Platform
 {
     public class TaskResponse
     {
+        /// <summary>
+        /// For queries with multi-page responses, use the page_number parameter to return data for specific pages. Data for page 1 is returned if this parameter is not included. 
+        /// </summary>
         [JsonProperty("page_number")]
         public int? PageNumber { get; set; } = 1;
-
+        /// <summary>
+        /// Use this parameter if total_pages indicates that there is more than one page of data available.
+        /// </summary>
         [JsonProperty("total_pages")]
         public int? TotalPages { get; set; } = 1;
 
+        /// <summary>
+        ///  List of tasks 
+        /// </summary>
         [JsonProperty("tasks")] 
         public List<Task> Tasks { get; set; } = new List<Task>(); 
     }
--- MoxiWorks/MoxiWorks.Platform/TaskService.cs
+++ MoxiWorks/MoxiWorks.Platform/TaskService.cs
@@ -4,6 +4,10 @@ using MoxiWorks.Platform.Interfaces;
 
 namespace MoxiWorks.Platform
 {
+    /// <summary>
+    /// Moxi Works Platform Task entities represent tasks that agents need to perform on behalf of, 
+    /// or in relation to their contacts.
+    /// </summary>
     public class TaskService : ITaskService
     {
         private readonly MoxiWorksClient Client; 
@@ -13,6 +17,30 @@ namespace MoxiWorks.Platform
             Client = client; 
         }
 
+        /// <summary>
+        /// Returns the specified Task if it exist.
+        /// </summary>
+        /// <param name="agentId">
+        /// Must include either:
+        /// AgentUuid
+        /// This is the Moxi Works Platform ID of the agent which an Group entry is associated 
+        /// with. This will be an RFC 4122 compliant UUID. 
+        /// agent_uuid or moxi_works_agent_id is required and must reference a 
+        /// valid Moxi Works Agent ID for your Group request to be accepted.
+        ///
+        /// MoxiWorksAgentId
+        /// This is the Moxi Works Platform ID of the agent which an Group entry is associated 
+        /// with. This will be a string that may take the form of an email address, 
+        /// or a unique identification string. agent_uuid or moxi_works_agent_id is required 
+        /// and must reference a valid Moxi Works Agent ID for your Group request to be accepted.
+        /// Agent ID for your Group request to be accepted.
+        /// </param>
+        /// <param name="agentIdType">What agentId type you are using.</param>
+        /// <param name="partnerContactId">
+        /// This is the unique identifer you use in your system that has been associated with the Contact that this Task regards.
+        /// </param>
+        /// <param name="partnerTaskId"></param>
+        /// <returns>Task Response emptu if task does not exist</returns>
         public async Task<Response<Task>> GetTaskAsync(string agentId, AgentIdType agentIdType, string partnerContactId,
             string partnerTaskId)
         {
@@ -23,18 +51,57 @@ namespace MoxiWorks.Platform
             return await Client.GetRequestAsync<Task>(builder.GetUrl()); 
         }
 
+        /// <summary>
+        /// update an existing task
+        /// </summary>
+        /// <param name="task">
+        /// The Task you want to update
+        /// </param>
+        /// <returns>The Response containing the update Task </returns>
         public async  Task<Response<Task>> UpdateTaskAsync(Task task)
         {
             var builder = new UriBuilder($"task/{task.PartnerTaskId}");
             return  await Client.PutRequestAsync(builder.GetUrl(),task);
         }
-
+        
+            
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task">the task you want to create.</param>
+        /// <returns>The Response containing the created Task </returns>
         public async Task<Response<Task>> CreateTaskAsync(Task task)
         {
             var builder = new UriBuilder($"task");
             return await Client.PostRequestAsync(builder.GetUrl(), task);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="agentId">
+        /// Must include either:
+        /// AgentUuid
+        /// This is the Moxi Works Platform ID of the agent which an Group entry is associated 
+        /// with. This will be an RFC 4122 compliant UUID. 
+        /// agent_uuid or moxi_works_agent_id is required and must reference a 
+        /// valid Moxi Works Agent ID for your Group request to be accepted.
+        ///
+        /// MoxiWorksAgentId
+        /// This is the Moxi Works Platform ID of the agent which an Group entry is associated 
+        /// with. This will be a string that may take the form of an email address, 
+        /// or a unique identification string. agent_uuid or moxi_works_agent_id is required 
+        /// and must reference a valid Moxi Works Agent ID for your Group request to be accepted.
+        /// Agent ID for your Group request to be accepted.
+        /// </param>
+        /// <param name="agentIdType">What agentId type you are using.</param>
+        /// <param name="startDate">Date to start Searching</param>
+        /// <param name="endDate">Date to end Search</param>
+        /// <param name="partnerContactId">   
+        /// This is the unique identifer you use in your system that has been associated with the Contact that this Task regards.
+        /// </param>
+        /// <param name="pageNumber">The page you want to view.</param>
+        /// <returns>Response with a list of Task in a Task Response object </returns>
         public async Task<Response<TaskResponse>> GetTaskDueBetweenAsync(string agentId, AgentIdType agentIdType, DateTime startDate,
             DateTime endDate, string partnerContactId, int pageNumber =1)
         {
@@ -50,3 +117,4 @@ namespace MoxiWorks.Platform
 
     }
 }
+ 
